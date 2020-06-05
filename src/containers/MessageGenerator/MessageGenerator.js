1f8ab29df8ec232d598c8bffcd93bdf318ffff55
bsd <datta.bathina15@gmail.com>
2020-06-05 22:53:24 +0530

    Added routing

--- src/containers/MessageGenerator/MessageGenerator.js
+++ src/containers/MessageGenerator/MessageGenerator.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Box } from 'grommet';
+import Message from '../../components/Message/Message';
+import MessageForm from '../../components/MessageGeneratorForm/MessageGeneratorForm';
 
 const MessageGenerator = () => {
-	return <div>
-        MessageGenerator
-    </div>;
+	const [messageData, setMessageData] = useState({
+		date: new Date(),
+		class: '',
+		rollNumber: 0,
+		numberOfPresent: 0,
+		numberOfAbsent: 0,
+		topicCovered: '',
+		assignment: '',
+	});
+	return (
+		<Box alignSelf="center" background="light-1" pad="small">
+			<Switch>
+				<Route path="/message">
+					<Message />
+				</Route>
+				<Route exact path="/">
+					<MessageForm messageData={messageData} />
+				</Route>
+			</Switch>
+		</Box>
+	);
 };
 
 export default MessageGenerator;
