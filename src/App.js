eeff8b185c495c204a76cd866078dca03eea2b20
bsd <datta.bathina15@gmail.com>
2020-06-05 22:52:44 +0530

    Added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { Grommet, Main, Heading } from 'grommet';
 import MessageGenerator from './containers/MessageGenerator/MessageGenerator';
 function App() {
@@ -12,14 +13,16 @@ function App() {
 		},
 	};
 	return (
-		<Grommet theme={theme}>
-			<Main pad="large">
-        <Heading alignSelf="center" size="small" color="brand">
-          Daywise Report Message Generator
-        </Heading>
-				<MessageGenerator />
-			</Main>
-		</Grommet>
+		<BrowserRouter>
+			<Grommet theme={theme}>
+				<Main pad="large">
+					<Heading alignSelf="center" size="small" color="brand">
+						Daywise Report Message Generator
+					</Heading>
+					<MessageGenerator />
+				</Main>
+			</Grommet>
+		</BrowserRouter>
 	);
 }
 
