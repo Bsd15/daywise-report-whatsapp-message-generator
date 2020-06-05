3f1cbda3265e256f039141f4d4e957b079b0cc19
bsd <datta.bathina15@gmail.com>
2020-06-06 00:12:17 +0530

    Created input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormField, TextInput, TextArea } from 'grommet';
+
+const Input = (props) => {
+	let input = null;
+	switch (props.type) {
+		case 'number':
+			input = (
+				<FormField label={props.label} name={props.name} pad>
+					<TextInput name={props.name} type="number" />
+				</FormField>
+			);
+			break;
+		case 'text-area':
+			input = (
+				<FormField label={props.label} name={props.name}>
+					<TextArea name={props.name} />
+				</FormField>
+			);
+			break;
+		case 'input':
+			input = (
+				<FormField label={props.label} name={props.name}>
+					<TextInput name={props.name} />
+				</FormField>
+			);
+			break;
+		case 'date':
+			input = (
+				<FormField label={props.label} name={props.name}>
+					<TextInput name={props.name} type="date"/>
+				</FormField>
+			);
+			break;
+		default:
+			input = null;
+			break;
+	}
+	return input;
+};
+
+Input.propTypes = {
+	type: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	min: PropTypes.number,
+	max: PropTypes.number,
+};
+
+export default Input;
