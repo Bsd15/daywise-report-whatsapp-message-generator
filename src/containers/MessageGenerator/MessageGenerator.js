9914625142aa71707e377ca8f7dceec5a2a802f6
bsd <datta.bathina15@gmail.com>
2020-06-06 00:12:39 +0530

    Modified messageData state to includ formData

--- src/containers/MessageGenerator/MessageGenerator.js
+++ src/containers/MessageGenerator/MessageGenerator.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 import { Box } from 'grommet';
 import Message from '../../components/Message/Message';
@@ -6,13 +7,38 @@ import MessageForm from '../../components/MessageGeneratorForm/MessageGeneratorF
 
 const MessageGenerator = () => {
 	const [messageData, setMessageData] = useState({
-		date: new Date(),
-		class: '',
-		rollNumber: 0,
-		numberOfPresent: 0,
-		numberOfAbsent: 0,
-		topicCovered: '',
-		assignment: '',
+		values: {
+			date: new Date(),
+			class: '',
+			rollNumber: 0,
+			numberOfPresent: 0,
+			numberOfAbsent: 0,
+			topicCovered: '',
+			assignment: ''
+		},
+		formData: {
+			date: {
+				type: "date"
+			},
+			class: {
+				type: "input"
+			},
+			rollNumber: {
+				type: "number",
+			},
+			numberOfPresent: {
+				type: "number"
+			},
+			numberOfAbsent: {
+				type: "number"
+			},
+			topicCovered: {
+				type: "text-area"
+			},
+			assignment: {
+				type: "text-area"
+			}
+		}
 	});
 	return (
 		<Box alignSelf="center" background="light-1" pad="small">
@@ -27,5 +53,12 @@ const MessageGenerator = () => {
 		</Box>
 	);
 };
+
+MessageGenerator.propTypes={
+	values: PropTypes.object,
+	formData: PropTypes.shape({
+		type: PropTypes.string
+	})
+}
 
 export default MessageGenerator;
