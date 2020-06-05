a2b4b9554d81fc1e058da50e3366d431c758ce68
bsd <datta.bathina15@gmail.com>
2020-06-05 22:54:22 +0530

    Created Message component to render final message

---
+++ src/components/Message/Message.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Message = () => {
+	return (
+		<div>
+			<p>Friday, 05-06-2020</p>
+			<p>
+				<b>Daywise report</b> of online class for VIII
+			</p>
+			<p>No. on roll: 5</p>
+			<p>No. on present: 5</p>
+			<p>No. on absent: NIL</p>
+			<p>Topic covered: Definition of Happiness</p>
+			<p>
+				Things assignment: Listing People/Things they like/Dislike with reasons
+			</p>
+			<p>Remarks: NIL</p>
+		</div>
+	);
+};
+
+export default Message;
