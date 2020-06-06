298d6a0c8fb8d6911a70ae3e576c018b8c183661
bsd <datta.bathina15@gmail.com>
2020-06-06 11:09:02 +0530

    Added Button to 'copy to clipboard'

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import format from 'date-fns/format';
+import { Button, Box } from 'grommet';
 
 const Message = (props) => {
 	const messageDate = new Date(props.messageData.date);
+	const ref = useRef();
+	const copyMessageToClipboard = () => {
+		console.log(ref.current.textContent());
+	};
 	return (
-		<div>
-			<p>
-				{format(messageDate, 'EEEE')}, {format(messageDate, 'dd-MM-yyyy')}
-			</p>
-			<p>
-				<b>Daywise report</b> of online class for {props.messageData.class}
-			</p>
-			<p>No. on roll: {props.messageData.rollNumber}</p>
-			<p>No. present: {props.messageData.numberOfPresent}</p>
-			<p>No. absent: {props.messageData.numberOfAbsent}</p>
-			<p>Topic covered: {props.messageData.topicCovered}</p>
-			<p>Home assignment: {props.messageData.assignment}</p>
-			<p>
-				Remarks: {props.messageData.remarks ? props.messageData.remarks : 'NIL'}
-			</p>
-		</div>
+		<Box>
+			<div ref={ref}>
+				<p>
+					{format(messageDate, 'EEEE')}, {format(messageDate, 'dd-MM-yyyy')}
+				</p>
+				<p>
+					<b>Daywise report</b> of online class for {props.messageData.class}
+				</p>
+				<p>No. on roll: {props.messageData.rollNumber}</p>
+				<p>No. present: {props.messageData.numberOfPresent}</p>
+				<p>No. absent: {props.messageData.numberOfAbsent}</p>
+				<p>Topic covered: {props.messageData.topicCovered}</p>
+				<p>Home assignment: {props.messageData.assignment}</p>
+				<p>
+					Remarks:{' '}
+					{props.messageData.remarks ? props.messageData.remarks : 'NIL'}
+				</p>
+			</div>
+			<Box>
+				<Button
+					label="Copy Text To Clipboard!"
+					onClick={copyMessageToClipboard}
+				/>
+			</Box>
+		</Box>
 	);
 };
 
