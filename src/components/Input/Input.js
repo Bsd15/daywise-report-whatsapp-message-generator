1970921a6a60789b0b9cef9ae451575bd01e0fb5
bsd <datta.bathina15@gmail.com>
2020-06-06 12:29:15 +0530

    Added 'time' input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -7,29 +7,57 @@ const Input = (props) => {
 	switch (props.type) {
 		case 'number':
 			input = (
-				<FormField label={props.label} name={props.name} pad required={props.required}>
+				<FormField
+					label={props.label}
+					name={props.name}
+					pad
+					required={props.required}
+				>
 					<TextInput name={props.name} type="number" />
 				</FormField>
 			);
 			break;
 		case 'text-area':
 			input = (
-				<FormField label={props.label} name={props.name} required={props.required}>
+				<FormField
+					label={props.label}
+					name={props.name}
+					required={props.required}
+				>
 					<TextArea name={props.name} />
 				</FormField>
 			);
 			break;
 		case 'input':
 			input = (
-				<FormField label={props.label} name={props.name} required={props.required}>
+				<FormField
+					label={props.label}
+					name={props.name}
+					required={props.required}
+				>
 					<TextInput name={props.name} />
 				</FormField>
 			);
 			break;
 		case 'date':
 			input = (
-				<FormField label={props.label} name={props.name} required={props.required}>
-					<TextInput name={props.name} type="date"/>
+				<FormField
+					label={props.label}
+					name={props.name}
+					required={props.required}
+				>
+					<TextInput name={props.name} type="date" />
+				</FormField>
+			);
+			break;
+		case 'time':
+			input = (
+				<FormField
+					label={props.label}
+					name={props.name}
+					required={props.required}
+				>
+					<TextInput name={props.name} type="time" />
 				</FormField>
 			);
 			break;
@@ -45,13 +73,13 @@ Input.propTypes = {
 	label: PropTypes.string.isRequired,
 	name: PropTypes.string.isRequired,
 	min: PropTypes.number,
-    max: PropTypes.number,
-    required: PropTypes.bool
+	max: PropTypes.number,
+	required: PropTypes.bool,
 };
 
 Input.defaultProps = {
-    min: 1,
-    required: false
-}
+	min: 1,
+	required: false,
+};
 
 export default Input;
